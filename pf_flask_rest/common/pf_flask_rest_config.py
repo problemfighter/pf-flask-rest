35e9f897e9f6a088e0b55d1adadcd3e05880358e
hmtmcse <hmtm.cse@gmail.com>
2022-01-14 01:34:53 +0600

    configure list pull

--- pf_flask_rest/common/pf_flask_rest_config.py
+++ pf_flask_rest/common/pf_flask_rest_config.py
@@ -9,3 +9,13 @@ class PFFRConfig:
     forbidden_redirect_url = "/not-allowed"
     api_url_start_with = "api"
     json_root_node = "data"
+
+    # API Config
+    get_page_param: str = "page"
+    item_per_page_param: str = "per-page"
+    sort_field_param: str = "sort-field"
+    sort_order_param: str = "sort-order"
+    search_field_param: str = "search"
+    sort_default_order: str = "desc"
+    sort_default_field: str = "id"
+    total_item_per_page: int = 25
