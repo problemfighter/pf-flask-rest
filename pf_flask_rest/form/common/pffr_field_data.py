2a3229a8ced6a1358cee71b3707f14267cfc1f3d
hmtmcse <hmtm.cse@gmail.com>
2022-02-24 14:23:27 +0600

    dadded form

--- pf_flask_rest/form/common/pffr_field_data.py
+++ pf_flask_rest/form/common/pffr_field_data.py
@@ -1,8 +1,10 @@
 class FieldData(object):
     name: str = None
     label: str = None
+    inputType: str = None
     value = ""
     default = None
     errors: str = None
     required: bool = False
     has_error: bool = False
+    helpText: str = None
