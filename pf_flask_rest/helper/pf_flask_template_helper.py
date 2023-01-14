5c272c752bc4be9a086387f6e30e276f185052ff
hmtmcse <hmtm.cse@gmail.com>
2023-01-15 00:37:27 +0600

    added util functionality

--- pf_flask_rest/helper/pf_flask_template_helper.py
+++ pf_flask_rest/helper/pf_flask_template_helper.py
@@ -1,5 +1,31 @@
-from flask import render_template
+import json
+from flask import render_template, get_flashed_messages
 from pf_flask_rest.form.pf_app_form_def import FormBaseDef
+
+
+class TemplateUtil:
+
+    def get_status_message(self):
+        messages = get_flashed_messages(with_categories=True)
+        response = {}
+        message_stack = ""
+        for status, message in messages:
+            if not status or not message:
+                continue
+            if status == "success":
+                response["isSuccess"] = True
+            elif "isSuccess" not in response:
+                response["isSuccess"] = False
+
+            if message:
+                message_stack += message + " "
+
+        if response and message_stack:
+            response["message"] = message_stack.strip()
+        return json.dumps(response)
+
+
+template_util = TemplateUtil()
 
 
 class TemplateHelper:
@@ -7,4 +33,5 @@ class TemplateHelper:
     def render(self, name, params={}, form: FormBaseDef = None):
         if form and form.definition:
             params["form"] = form.definition
+        params["util"] = template_util
         return render_template(f"{name}.html", **params)
